40218424c46a468a0435bdfc7120cd7a1d81dcc9
Karina Kumykova <k.kumykova@gmail.com>
2022-02-22 11:28:02 +0000

    define class Hat object with var nums of args; add them to a contents instance variable

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,26 @@
 import copy
 import random
+
+
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+        self.contents = []
+        for (k, v) in kwargs.items():
+            if len(kwargs) > 0:
+                self.contents.append("".join((k+",")*v).split(","))
+        self.contents = [string for ball_list in self.contents for string in ball_list if (string != "")]
+        print(self.contents)
+
+
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
+
+
+if __name__ == "__main__":
+    hat1 = Hat(red=3, blue=2)
